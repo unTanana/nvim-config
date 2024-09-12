75344992ad12f786695f6eb87155e75d4d654304
Ciprian Tanana <ciprian.tanana@gmail.com>
2024-09-12 11:06:16 +0300

    change telescope layout

--- after_backup/plugin/lsp.lua
+++ after_backup/plugin/lsp.lua
@@ -183,6 +183,7 @@ lsp.set_preferences({
 lsp.on_attach(function(_, bufnr)
     local opts = { buffer = bufnr, remap = false }
     vim.keymap.set("n", "gd", function() vim.lsp.buf.definition() end, opts)
+    vim.keymap.set("n", "gi", function() vim.lsp.buf.implementation() end, opts)
     vim.keymap.set("n", "K", function() vim.lsp.buf.hover() end, opts)
     vim.keymap.set("n", "<leader>lf", format.do_format, opts)
     vim.keymap.set("n", "]d", function() vim.diagnostic.goto_next() end, opts)
--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -1,29 +1,43 @@
 return {
-    {
-        "nvim-telescope/telescope-ui-select.nvim",
-    },
-    {
-        "nvim-telescope/telescope.nvim",
-        tag = "0.1.5",
-        dependencies = { "nvim-lua/plenary.nvim" },
-        config = function()
-            require("telescope").setup({
-                extensions = {
-                    ["ui-select"] = {
-                        require("telescope.themes").get_dropdown({}),
-                    },
-                },
-            })
-            local builtin = require("telescope.builtin")
+	{
+		"nvim-telescope/telescope-ui-select.nvim",
+	},
+	{
+		"nvim-telescope/telescope.nvim",
+		tag = "0.1.5",
+		dependencies = { "nvim-lua/plenary.nvim" },
+		config = function()
+			require("telescope").setup({
+				defaults = {
+					layout_strategy = "vertical",
+					layout_config = {
+						vertical = {
+							preview_width = 0.6, -- Adjust the preview window size
+							mirror = false, -- Don't mirror the layout
+						},
+					},
+				},
+				extensions = {
+					["ui-select"] = {
+						require("telescope.themes").get_dropdown({}),
+					},
+				},
+				pickers = {
+					find_files = {
+						hidden = true,
+					},
+				},
+			})
+			local builtin = require("telescope.builtin")
 
-            vim.keymap.set("n", "<leader><leader>", builtin.oldfiles, {})
-            vim.keymap.set('n', '<leader>sf', builtin.find_files, {})
-            vim.keymap.set('n', '<leader>sr', builtin.resume, {})
-            vim.keymap.set('n', '<leader>sg', builtin.git_files, {})
-            vim.keymap.set('n', '<leader>f', builtin.live_grep, {})
-            vim.keymap.set('n', '<leader>sw', '<cmd>Telescope lsp_dynamic_workspace_symbols<CR>')
+			vim.keymap.set("n", "<leader><leader>", builtin.oldfiles, {})
+			vim.keymap.set("n", "<leader>sf", builtin.find_files, {})
+			vim.keymap.set("n", "<leader>sr", builtin.resume, {})
+			vim.keymap.set("n", "<leader>sg", builtin.git_files, {})
+			vim.keymap.set("n", "<leader>f", builtin.live_grep, {})
+			vim.keymap.set("n", "<leader>sw", "<cmd>Telescope lsp_dynamic_workspace_symbols<CR>")
 
-            require("telescope").load_extension("ui-select")
-        end,
-    },
+			require("telescope").load_extension("ui-select")
+		end,
+	},
 }
