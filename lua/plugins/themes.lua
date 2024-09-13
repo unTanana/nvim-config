183b41656e03dc18a849cb69ffee92112bef4fee
Ciprian Tanana <ciprian.tanana@gmail.com>
2024-09-13 10:10:15 +0300

    theme update

--- lua/plugins/themes.lua
+++ lua/plugins/themes.lua
@@ -10,21 +10,21 @@ return {
     },
     {
         "Mofiqul/vscode.nvim",
-        lazy = true,
-        -- name = "vscode",
-        -- priority = 1000,
-        -- config = function()
-        -- 	vim.o.background = "dark"
-        -- 	require("vscode").setup({
-        -- 		-- transparent = true,
-        -- 		-- Enable italic comment
-        -- 		italic_comments = true,
-        -- 		-- Underline `@markup.link.*` variants
-        -- 		underline_links = true,
-        -- 	})
-        -- 	-- load the theme without affecting devicon colors.
-        -- 	vim.cmd.colorscheme("vscode")
-        -- end,
+        lazy = false,
+        name = "vscode",
+        priority = 1000,
+        config = function()
+        	vim.o.background = "dark"
+        	require("vscode").setup({
+        		transparent = true,
+        		-- Enable italic comment
+        		italic_comments = true,
+        		-- Underline `@markup.link.*` variants
+        		underline_links = true,
+        	})
+        	-- load the theme without affecting devicon colors.
+        	vim.cmd.colorscheme("vscode")
+        end,
     },
     {
         "AlexvZyl/nordic.nvim",
@@ -36,37 +36,37 @@ return {
     },
     {
         "ellisonleao/gruvbox.nvim",
-        lazy = false,
-        name = "gruvbox",
-        priority = 1000,
-        config = function()
-            -- Default options:
-        	vim.o.background = "dark"
-            require("gruvbox").setup({
-                terminal_colors = true, -- add neovim terminal colors
-                undercurl = true,
-                underline = true,
-                bold = true,
-                italic = {
-                    strings = true,
-                    emphasis = true,
-                    comments = true,
-                    operators = false,
-                    folds = true,
-                },
-                strikethrough = true,
-                invert_selection = false,
-                invert_signs = false,
-                invert_tabline = false,
-                invert_intend_guides = false,
-                inverse = true, -- invert background for search, diffs, statuslines and errors
-                contrast = "", -- can be "hard", "soft" or empty string
-                palette_overrides = {},
-                overrides = {},
-                dim_inactive = false,
-                transparent_mode = true,
-            })
-            vim.cmd.colorscheme("gruvbox")
-        end,
+        lazy = true,
+        -- name = "gruvbox",
+        -- priority = 1000,
+        -- config = function()
+        --     -- Default options:
+        -- 	vim.o.background = "dark"
+        --     require("gruvbox").setup({
+        --         terminal_colors = true, -- add neovim terminal colors
+        --         undercurl = true,
+        --         underline = true,
+        --         bold = true,
+        --         italic = {
+        --             strings = true,
+        --             emphasis = true,
+        --             comments = true,
+        --             operators = false,
+        --             folds = true,
+        --         },
+        --         strikethrough = true,
+        --         invert_selection = false,
+        --         invert_signs = false,
+        --         invert_tabline = false,
+        --         invert_intend_guides = false,
+        --         inverse = true, -- invert background for search, diffs, statuslines and errors
+        --         contrast = "", -- can be "hard", "soft" or empty string
+        --         palette_overrides = {},
+        --         overrides = {},
+        --         dim_inactive = false,
+        --         transparent_mode = true,
+        --     })
+        --     vim.cmd.colorscheme("gruvbox")
+        -- end,
     },
 }
