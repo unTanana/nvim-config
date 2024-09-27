6f6641273229590046ef80d5c60ab9cdcb623450
Ciprian Tanana <ciprian.tanana@gmail.com>
2024-09-27 19:42:05 +0300

    oil fix

--- lua/plugins/oil.lua
+++ lua/plugins/oil.lua
@@ -1,8 +1,8 @@
 return {
-  "stevearc/oil.nvim",
-  config = function()
-    local oil = require("oil")
-    oil.setup()
-    vim.keymap.set("n", "-", oil.toggle_float, {})
-  end,
+	"stevearc/oil.nvim",
+	config = function()
+		local oil = require("oil")
+		oil.setup()
+		vim.keymap.set("n", "<leader>o", oil.toggle_float, {})
+	end,
 }
