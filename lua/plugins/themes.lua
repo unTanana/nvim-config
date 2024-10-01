5eb3978b144db0501b22f877371a6908737eb7c8
Ciprian Tanana <ciprian.tanana@gmail.com>
2024-10-01 17:43:40 +0300

    fixes

--- lua/plugins/themes.lua
+++ lua/plugins/themes.lua
@@ -1,30 +1,30 @@
 return {
     {
-        "catppuccin/nvim"
-        -- lazy = false,
-        -- name = "catppuccin",
-        -- priority = 1000,
-        -- config = function()
-        --     vim.cmd.colorscheme("catppuccin-mocha")
-        -- end,
+        "catppuccin/nvim",
+        lazy = false,
+        name = "catppuccin",
+        priority = 1000,
+        config = function()
+            vim.cmd.colorscheme("catppuccin-mocha")
+        end,
     },
     {
         "Mofiqul/vscode.nvim",
-        lazy = false,
-        name = "vscode",
-        priority = 1000,
-        config = function()
-        	vim.o.background = "dark"
-        	require("vscode").setup({
-        		transparent = true,
-        		-- Enable italic comment
-        		italic_comments = true,
-        		-- Underline `@markup.link.*` variants
-        		underline_links = true,
-        	})
-        	-- load the theme without affecting devicon colors.
-        	vim.cmd.colorscheme("vscode")
-        end,
+        -- lazy = false,
+        -- name = "vscode",
+        -- priority = 1000,
+        -- config = function()
+        -- 	vim.o.background = "dark"
+        -- 	require("vscode").setup({
+        -- 		transparent = true,
+        -- 		-- Enable italic comment
+        -- 		italic_comments = true,
+        -- 		-- Underline `@markup.link.*` variants
+        -- 		underline_links = true,
+        -- 	})
+        -- 	-- load the theme without affecting devicon colors.
+        -- 	vim.cmd.colorscheme("vscode")
+        -- end,
     },
     {
         "AlexvZyl/nordic.nvim",
