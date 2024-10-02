c3f47664e73c19fc2e2c11b05f85e8106c3911aa
Ciprian Tanana <ciprian.tanana@gmail.com>
2024-10-02 14:52:49 +0300

    terminal keymaps for normal mode

--- lua/plugins/toggleterm.lua
+++ lua/plugins/toggleterm.lua
@@ -1,17 +1,29 @@
 return {
-  "akinsho/toggleterm.nvim",
-    config = function()
-      require("toggleterm").setup({
-          open_mapping = [[<c-\>]],
-          direction = "float",
-          float_opts = {
-              border = "curved",
-              winblend = 0,
-              highlights = {
-                  border = "Normal",
-                  background = "Normal",
-              },
-          },
-      })
-    end
+	"akinsho/toggleterm.nvim",
+	config = function()
+		require("toggleterm").setup({
+			open_mapping = [[<c-\>]],
+			direction = "float",
+			float_opts = {
+				border = "curved",
+				winblend = 0,
+				highlights = {
+					border = "Normal",
+					background = "Normal",
+				},
+			},
+		})
+
+		function _G.set_terminal_keymaps()
+			local opts = { noremap = true }
+			-- Check if the current terminal is running lazygit
+			local term_buf_name = vim.api.nvim_buf_get_name(0)
+			if not string.find(term_buf_name, "lazygit") then
+				vim.api.nvim_buf_set_keymap(0, "t", "<esc>", [[<C-\><C-n>]], opts)
+				vim.api.nvim_buf_set_keymap(0, "t", "jk", [[<C-\><C-n>]], opts)
+			end
+		end
+
+		vim.cmd("autocmd! TermOpen term://* lua set_terminal_keymaps()")
+	end,
 }
